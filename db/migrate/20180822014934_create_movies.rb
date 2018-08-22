467f36138978e59cf87d9cae208c71912fb77756
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-08-22 02:14:56 +0000

    automatically backed up by learn

--- db/migrate/20180822014934_create_movies.rb
+++ db/migrate/20180822014934_create_movies.rb
@@ -11,14 +11,14 @@ class CreateMovies < ActiveRecord::Migration[5.2]
 end
 
 
-# class CreateMovies < ActiveRecord::Migration[5.2]
-#   def change
-#     create_table :movies do |t|
-#       t.string :title
-#       t.integer :release_date
-#       t.string :director
-#       t.string :lead
-#       t.boolean :in_theaters
-#     end
-#   end
-# end
+class CreateMovies < ActiveRecord::Migration[5.2]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.integer :release_date
+      t.string :director
+      t.string :lead
+      t.boolean :in_theaters
+    end
+  end
+end
